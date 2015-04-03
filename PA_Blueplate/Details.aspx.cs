6e6e7fd3d92a1f82085685ea520c6e805fecc5bb
mhouseman <mjhouseman@gmail.com>
2015-04-02 20:15:36 -0400

    Sync

--- PA_Blueplate/Details.aspx.cs
+++ PA_Blueplate/Details.aspx.cs
@@ -25,7 +25,15 @@ namespace PA_Blueplate
             switch (option)
             {
                 case "repairs" :
-                    
+                    lblBodyPartDiscount.Visible = true;
+                    lblBodyPartDiscountRate.Visible = true;
+                    lblBodyPartDiscountRateText.Visible = true;
+                    lblBodyPartDiscountText.Visible = true;
+                    lblBodyRepair.Visible = true;
+                    lblBodyRepairRate.Visible = true;
+                    lblBodyRepairRateText.Visible = true;
+                    lblBodyRepairText.Visible = true;
+                    lblCityText.Visible = true;
                     break;
                 case "towing" :
                     lblPhone.Visible = false;
